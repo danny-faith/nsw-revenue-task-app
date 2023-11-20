1782d2d3cb3b46af804717c02ac05749e3c85477
daniel blythe <daniel.blythe@oohmedia.com.au>
2023-11-21 08:12:33 +1100

    simple link to image viewer page

---
+++ src/app/image-viewer/page.tsx
@@ -0,0 +1,27 @@
+import { Inter } from "next/font/google";
+
+import Link from "next/link";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function ImageViewer() {
+  return (
+    <div className={`${inter.className}`}>
+      <p>Image viewer</p>
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <Link
+          href="/"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Home{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Return home</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
