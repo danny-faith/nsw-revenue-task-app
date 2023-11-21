10e7abde9d84080e3a22fd342e2538d072716fb4
daniel blythe <daniel.blythe@oohmedia.com.au>
2023-11-22 08:39:57 +1100

    added constants file and meta data to images

--- src/app/components/Image.tsx
+++ src/app/components/Image.tsx
@@ -1,15 +1,21 @@
 "use client";
 import Image from "next/image";
 import { PropsImage } from "../image-viewer/page";
+import { CONST } from "../constants";
 
-function ImageBlock({ id }: PropsImage) {
+function ImageBlock({ id, author, height, width }: PropsImage) {
   return (
-    <Image
-      src={`https://picsum.photos/id/${id}/367/267`}
-      alt={""}
-      width={367}
-      height={267}
-    />
+    <div>
+      <Image
+        src={`https://picsum.photos/id/${id}/${CONST.IMAGE_PREVIEW_WIDTH}/${CONST.IMAGE_PREVIEW_HEIGHT}`}
+        alt={author}
+        width={367}
+        height={267}
+      />
+      <p className="text-sm">Author: {author}</p>
+      <p className="text-sm">Width: {width}</p>
+      <p className="text-sm">Height: {height}</p>
+    </div>
   );
 }
 
