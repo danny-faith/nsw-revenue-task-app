1a1643ca74ad5c0647a808fe625641b6b1da7780
daniel blythe <daniel.blythe@oohmedia.com.au>
2023-11-22 09:46:42 +1100

    removed uneccesary font

--- src/app/image-viewer/page.tsx
+++ src/app/image-viewer/page.tsx
@@ -1,13 +1,9 @@
 "use client";
-import { Inter } from "next/font/google";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ImageList } from "../components/ImageList";
 import { SearchImage } from "../components/SearchImage";
 import { ImageListProvider } from "../context/ImageListContext";
 import { useSession } from "next-auth/react";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export type PropsImage = {
   id: string;
@@ -18,7 +14,7 @@ export type PropsImage = {
   download_url: string;
 };
 
-export default async function ImageViewer() {
+export default function ImageViewer() {
   const session = useSession();
 
   if (!session) {
@@ -27,20 +23,8 @@ export default async function ImageViewer() {
 
   return (
     <ImageListProvider>
-      <div className={`${inter.className}`}>
-        <p>Image viewer</p>
-        <Link
-          href="/"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Home{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>Return home</p>
-        </Link>
+      <div>
+        <h1 className="text-center text-2xl mb-10">Image viewer</h1>
         <SearchImage />
         <ImageList />
       </div>
