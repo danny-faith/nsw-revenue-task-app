d37a76a787e6cea910d672cf453bf844dd30cf6b
daniel blythe <daniel.blythe@oohmedia.com.au>
2023-11-22 09:40:09 +1100

    added github avart url

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         hostname: "picsum.photos",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+      },
     ],
   },
 };
