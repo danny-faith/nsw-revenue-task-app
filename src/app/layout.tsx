beb64ec49a197948bbc9519657948300633e2d56
daniel blythe <daniel.blythe@oohmedia.com.au>
2023-11-22 14:29:47 +1100

    moved session provider to correct place

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import SessionProvider from "./components/SessionProvider";
+import SessionProvider from "./providers/SessionProvider";
 import { getServerSession } from "next-auth";
 import NavMenu from "./components/Navigation";
 import { ReactQueryProvider } from "./providers/ReactQueryProvider";
